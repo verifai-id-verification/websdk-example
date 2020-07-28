2b58bbb2455c455ac1656c2d4b74d0af05a59e9f
carolien.braams <carolien.braams@verifai.com>
2020-07-28 08:45:45 +0200

    v1 of quickstart

--- README.md
+++ README.md
@@ -1,3 +1,86 @@
-# websdk-exampleapp
+# Verifai web SDK
 
-A example app on how to integrate the websdk into your own application
+This is the Verifai webSDK example project.
+
+It exists of two parts:
+
+* Example Backend
+* Example Frontend
+
+## Running the example Backend
+
+You can run the example backend by:
+
+```bash
+cd backend
+# install the requirements
+pip3 install -r requirements.txt
+# run the webserver
+python3 main.py
+```
+
+You can check if the server is running by going to:
+http://0.0.0.0:8091/token
+
+Here you should be greeted with some json.
+
+## Running the frontend
+
+For the frontend you need to serve the static files in the `frontend` folder:
+
+We use `serve` for this: https://www.npmjs.com/package/serve .
+But any other static file server like `apache` or `ngnix` will do.
+
+Depending on your platform you can do:
+```bash
+npm install -g serve
+```
+
+Then to run:
+```bash
+cd frontend
+serve .
+```
+
+`serve` uses port `5000` by default so when you go to: http://localhost:5000 you should be greeted by the Verifai webSDK
+
+## Folder contents
+
+### Backend
+Contains the example backend. This example contains the three methods you should implement in your backend when communicating with the Verifai middleware.
+
+There are 3 endpoints:
+
+* **[ /token ]** Is used to get the Verifai one time use token, This token can be used to start a user session to go through the SDK.
+* **[ /result ]** Is used to fetch the Verifai result from the Verifai temp storage. this endpoint should be called when the user session is finished i.e. in the `on_finished` method in the frontend.
+* **[ /remove_session ]** After the result is collected you should remove the session from the Verifai temporal storage to make sure there is no unnecessary (personal) data floating around in the temporal storage.
+
+These 3 endpoints are direct proxies to the middleware:
+
+| Endpoint                                | Method | Datatype | Result |
+|-----------------------------------------|--------|----------|--------|
+| /v1/auth/token                          | POST   | JSON     | JSON   |
+| /v1/session/{session_id}/verifai-result | GET    | -        | JSON   |
+| /v1/session/{session_id}                | DELETE | -        | JSON   |
+
+ **Q:** Why proxy and not call these endpoints directly from the frontend?
+
+ **A:** Because you need the shared secret to authorize, if the secret is exposed on the frontend anyone can call these endpoints which would be a _major_ security breach.
+
+
+### Frontend
+This folder contains an example implementation for the frontend. The files `/frontend/index.html` and `/frontend/example.js` are thoroughly documented and should be self-explanatory.
+
+## Known issues
+
+This list contains some known issues. We are working hard on these and you don't need to report them. Reports on all other issues you may find are very welcome.
+
+* The CSS is not very readable, documented, or modifiable yet.
+* Documentation other than found in this folder.
+* The error screen reset button does not always work.
+* Some older browser and phones are not yet supported.
+* SDK crashes when some buttons are fast repeatedly clicked.
+* Sometimes the cropping does not work.
+* Landscape mode on phones is not supported.
+* Duration of a session or handover is too short.
+* Session timeout does not display an error.
---
+++ backend/main.py
@@ -0,0 +1,127 @@
+# Verifai Example Backend
+#
+# This is an example for a clients backend.
+# The general idea is that this is a proxy to the Verifai Middleware.
+#
+# Here you should store the VERIFAI_INTERNAL_TOKEN.
+# The token is a shared secret between your own backend and the Verifai middleware.
+#
+#############################################################
+# DO NOT store the VERIFAI_INTERNAL_TOKEN in your frontend, #
+# this will expose your users data to the outside.          #
+#############################################################
+#
+#
+# This example is build with:
+#    * [python 3] https://www.python.org/ (language)
+#    * [flask]    https://flask.palletsprojects.com/en/1.1.x/ (web-server)
+#    * [requests] https://requests.readthedocs.io/en/master/ (HTTP lib)
+#
+# You can run this example for testing and demo purposes.
+# For production you should implement it yourself in your own web-application.
+
+import json
+import requests
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# Settings for the example backend
+HOST = '0.0.0.0'
+PORT = '8091'
+DEBUG = True
+
+# The shared secret with the Verifai middleware
+VERIFAI_INTERNAL_TOKEN = 'a20541118dbdd071fa0fec86d8ea44e99c5b9709f5d7cd82'
+# Url to the Verifai middleware
+MIDDLEWARE_URL = 'https://rug.websdk.verifai.io'
+# Authorization header
+HEADERS = {'Authorization': 'Bearer ' + VERIFAI_INTERNAL_TOKEN}
+# The url for the handover
+HANDOVER_BASE_URL = 'http://localhost:5000/#/handover?s='
+
+
+# Create a flask app
+app = Flask(__name__)
+
+# Add Cors to the app
+CORS(app)
+
+
+# Endpoint to get the Verifai One time use token
+# ---------------------------------------------------
+# Before you can start the Verifai websdk flow you need to get the one time use token.
+# Here the one time use token is requested from the middleware,
+# and returned to the frontend.
+# The return object also holds the 'backend_url', this is the url to the Verifai Middleware.
+# The websdk uses this url to communicate directly with the middleware.
+
+@app.route('/token')
+def get_token():
+    endpoint = f'{MIDDLEWARE_URL}/v1/auth/token'
+    data = {
+        "document_type_whitelist": json.dumps([  # You can include document types
+            "P",  # Passport
+            "I",  # ID-card
+            "D",  # Drivers license
+            "R",  # Refugee travel document
+            # "RP",  # Residence permit
+            # "V",  # Visa (not fully supported yet)
+        ]),
+        # The url to where Verifai should point during a handover.
+        # There should be a served Verifai SDK at this location connected to the same middleware as the initial SDK.
+        # In the example it points to where the example frontend is served.
+        "handover_base_url": HANDOVER_BASE_URL 
+
+        # "country_choices_blacklist": json.dumps(["DE", "IT"]),  # You can exclude countries
+        # "country_choices_whitelist": json.dumps(["NL"])  # Or exclusively include countries
+        # countries are denoted by their 2 letter country code:
+        # https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
+    }
+    response = requests.post(endpoint, data, headers=HEADERS)
+    content = json.loads(response.content)
+
+    verifai_token = content['token']
+    backend_url = MIDDLEWARE_URL + '/v1/'
+
+    return jsonify({
+        "verifai_token": verifai_token,
+        "backend_url": backend_url
+    }), 200
+
+
+# Endpoint to get the Verifai Result after the session has ended
+# -----------------------------------------------------------------
+# When the Verifai flows are finished, a session_id is returned in the `on_finished` callback in the frontend.
+# With this session_id you are able to fetch the Verifai result from the Verifai Middleware.
+# The result object holds all the information extracted during the flow.
+# https://verifai.dev/#result-processing gives an idea of what fields you can expect.
+# Note the url links to the result object for the Mobile SDK, there might be some minor differences.
+
+@app.route('/result/<string:session_id>')
+def get_result(session_id):
+    endpoint = f'{MIDDLEWARE_URL}/v1/session/{session_id}/verifai-result'
+    response = requests.get(endpoint, headers=HEADERS)
+    content = json.loads(response.content)
+    return jsonify(content), 200
+
+
+# Endpoint to remove the session from the Verifai temp memory after the session has ended.
+# -----------------------------------------------------------------------------------------
+# After the Verifai flow has finished you should remove the session from the Verifai temp storage.
+# This is done with the session id returned by the `on_finished` callback in the frontend.
+# When a session is deleted all related objects are also deleted from the Verifai storage.
+# You should be double sure the data is stored on your own system before calling this.
+# If this method is not called the data is automatically removed after 1 week.
+# The duration of the data persistence might change with newer releases.
+
+@app.route('/session/<string:session_id>', methods=['DELETE'])
+def remove_session(session_id):
+    endpoint = f'{MIDDLEWARE_URL}/v1/session/{session_id}'
+    response = requests.delete(endpoint, headers=HEADERS)
+    content = json.loads(response.content)
+    return jsonify(content), 200
+
+
+# starts the flask web-server
+if __name__ == "__main__":
+    app.run(host=HOST, port=PORT, debug=DEBUG)
